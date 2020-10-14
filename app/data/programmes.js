fdec99b919acb5cad36a70114ac604f1b667bef4
Ed Horsford <mail@edwardhorsford.com>
2020-10-14 16:40:48 +0100

    Initiall support for multiple routes

---
+++ app/data/programmes.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "Assessment Only": {
+    name: "Assessment Only",
+    enabled: true,
+    sections: [
+      'programmeDetails',
+      'personalDetails',
+      'contactDetails',
+      'diversity',
+      'degree'
+    ]
+  },
+  "Provider-led": {
+    name: "Provider-led",
+    enabled: true,
+    sections: [
+      'programmeDetails',
+      'personalDetails',
+      'contactDetails',
+      'diversity',
+      'degree'
+    ]
+  }
+}
+
+  
