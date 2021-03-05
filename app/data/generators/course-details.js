ed41395fe993945f70357e5eb6b91c0004ac3ca6
Ed Horsford <mail@edwardhorsford.com>
2021-03-05 13:17:28 +0000

    Rename programme details to course details

--- app/data/generators/course-details.js
+++ app/data/generators/course-details.js
@@ -7,30 +7,25 @@ const trainingRouteData = require('./../training-route-data')
 const ittSubjects = require('./../itt-subjects')
 const courses           = require('./../courses.json')
 
-const publishRoutes = [
-  'School direct salaried',
-  'School direct tuition fee',
-  'Apprenticeship PG',
-  'Provider-led'
-]
+const publishRoutes = trainingRouteData.publishRoutes
 
-const generateCourseDetails = require('./course-details')
+const generateCourse = require('./course-generator')
 
 module.exports = (params, application) => {
 
   // Arwkwardly work out if this should be a publish course
   // Todo: could this be rewritten?
   let isPublishCourse
-  let programmeDetails
+  let courseDetails
 
-  if (params?.programmeDetails?.isPublishCourse !== undefined){
-    isPublishCourse = params.programmeDetails.isPublishCourse
+  if (params?.courseDetails?.isPublishCourse !== undefined){
+    isPublishCourse = params.courseDetails.isPublishCourse
   }
   else isPublishCourse = publishRoutes.includes(application.route)
 
   // If a publish course, pick from seed courses
   if (isPublishCourse) {
-    // Grab programme details from seed courses
+    // Grab course details from seed courses
     let providerCourses = courses[application.provider].courses.filter(course => course.route == application.route)
 
     // Todo: seed courses for a provider might not align with selected or enabled routes. 
@@ -40,18 +35,18 @@ module.exports = (params, application) => {
       providerCourses = courses[application.provider].courses
     }
     // Pick a random course
-    programmeDetails = faker.helpers.randomize(providerCourses)
+    courseDetails = faker.helpers.randomize(providerCourses)
   }
   else {
     // Generate some seed data
-    let programmeDetailsOptions = {
+    let courseDetailsOptions = {
       route: application.route, 
       startYear: application.academicYear,
       isPublishCourse // Implicitly false
     }
 
-    programmeDetails = generateCourseDetails(programmeDetailsOptions)
+    courseDetails = generateCourse(courseDetailsOptions)
   }
 
-  return programmeDetails
+  return courseDetails
 }
